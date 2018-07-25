9d98cda94b03e944e6fb16927e57c8c211c635f2
likunyu <122365702@qq.com>
2018-07-25 11:02:19 +0800

    canvas->img“

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,95 +1,57 @@
+<style type="text/css">
+	.hello img{
+		margin: 20px auto;
+	}
+	#canvasImgDom{
+		padding: 10px;
+		width: 500px;
+		margin: 20px auto;
+		background-color: gray;
+		color: #fff;
+	}
+	#canvasImgDom ul{
+		width: 100%;
+		text-align: left;
+	}
+</style>
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div id="hello" class="hello" >
+  	<div id="canvasImgDom">
+  		<h2>完成思路：</h2>
+  		<ul>
+  			<li>1、利用<a href="http://html2canvas.hertzen.com/getting-started/" target="_blank">html2canvas</a> 插件，把html用canvas画出来；</li>
+  			<li>2、new Image()创建图片，插入到DOM中。</li>
+  		</ul>
+  	</div>
+  	<p>下面的就是生成的Img：</p>
   </div>
 </template>
-
 <script>
+	import html2canvas from 'html2canvas';
 export default {
   name: 'HelloWorld',
+   created () {
+      this.$nextTick(()=>{
+      	console.log($(".hello"))
+      	html2canvas(document.getElementById("canvasImgDom")).then(function(canvas) {
+      		var image = new Image();
+					image.src = canvas.toDataURL("image/png");
+//					$("#canvasImgDom").css('display',"none");
+				  document.getElementById("hello").appendChild(image);
+				});
+      })
+    },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods:{
+  	convertCanvasToImage (canvas) {
+			var image = new Image();
+			image.src = canvas.toDataURL("image/png");
+			return image;
+		}
   }
 }
 </script>
